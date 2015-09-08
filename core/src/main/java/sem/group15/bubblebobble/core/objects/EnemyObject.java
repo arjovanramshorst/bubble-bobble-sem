c98d7c2af90a639adcc7440a0ced2e8de72d32a0
Premmie <e.a.Dammeijer@student.tudelft.nl>
2015-09-08 12:07:07 +0200

    Add EnemeyObject class and how the speed of the enemy is handeld
    The speed of an enemey is 100 in the x-direction, if a wall is hit the direction will be changed by -1.

---
+++ core/src/main/java/sem/group15/bubblebobble/core/objects/EnemyObject.java
@@ -0,0 +1,54 @@
+package sem.group15.bubblebobble.core.objects;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+/**
+ * Created by TUDelft SID on 8-9-2015.
+ */
+public class EnemyObject extends GravityObject{
+
+    protected boolean wallCollision;
+
+
+    public EnemyObject(float xPosition, float yPosition) {
+        super(new Rectangle(32,32,xPosition,yPosition), new Texture(Gdx.files.internal("aqua-ball.png")));
+    }
+
+    @Override
+    public void update(float elapsed) {
+        super.update(elapsed);
+
+        if (!wallCollision) {
+            currentSpeedX = 100;
+        } else {
+            currentSpeedX = -1 * currentSpeedX;
+        }
+
+        location.x += currentSpeedX * elapsed;
+        location.y += currentSpeedY * elapsed;
+    }
+
+
+    @Override
+    public void checkCollision(GameObject other) {
+
+    }
+
+    @Override
+    public void handleCollision(GameObject other) { 
+        super.handleCollision(other);
+        if(other instanceof ImmutableObject) {
+
+        }
+
+    }
+
+    @Override
+    public void draw(SpriteBatch spriteBatch) {
+        spriteBatch.draw(texture, location.x, location.y);
+    }
+}
