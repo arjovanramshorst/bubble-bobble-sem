e22ffeda56cfefba8502ef3bbcc2f74cd7bea82c
Arjo <arjovanramshorst@gmail.com>
2015-09-12 00:32:11 +0200

    Quickfix BubbleObject constructor.

--- core/src/main/java/sem/group15/bubblebobble/core/objects/BubbleObject.java
+++ core/src/main/java/sem/group15/bubblebobble/core/objects/BubbleObject.java
@@ -19,7 +19,7 @@ public class BubbleObject extends FloatingObject {
      * @param direction 1 if player is look right, -1 if player is looking left.
      */
     public BubbleObject(float xPosition, float yPosition, int direction) {
-        super(new Rectangle(32,32,xPosition,yPosition), new Texture(Gdx.files.internal("aqua-ball.png")));
+        super(new Rectangle(xPosition,yPosition,32,32), new Texture(Gdx.files.internal("aqua-ball.png")));
         xSpeed = 30 * direction;
         ySpeed = 0;
     }
