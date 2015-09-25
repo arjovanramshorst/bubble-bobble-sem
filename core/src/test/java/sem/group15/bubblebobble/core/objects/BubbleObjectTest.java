b7d954dbf8c32c69c92c42a287279ca30d3e0aff
Wouter Zirkzee <W.J.Zirkzee@student.tudelft.nl>
2015-09-25 10:25:36 +0200

    bubbles get removed after a certain time, also tested <feature, testing

---
+++ core/src/test/java/sem/group15/bubblebobble/core/objects/BubbleObjectTest.java
@@ -0,0 +1,56 @@
+package sem.group15.bubblebobble.core.objects;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.Rectangle;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mockito;
+import sem.group15.bubblebobble.core.BubbleBobble;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by woute on 9/25/2015.
+ */
+public class BubbleObjectTest {
+
+    private BubbleObject bubble;
+
+    /**
+     * setup mokito for mocking
+     * @throws Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        Gdx.app = mock(Application.class);
+        Gdx.input = mock(Input.class);
+        bubble = Mockito.mock(BubbleObject.class, Mockito.CALLS_REAL_METHODS);
+        bubble.location = new Rectangle(BubbleBobble.SPRITE_SIZE - 2, 0, BubbleBobble.SPRITE_SIZE, BubbleBobble.SPRITE_SIZE);
+    }
+
+    @Test
+    public void testUpdateSpeed() {
+        bubble.xSpeed = 40;
+        bubble.update(0.1f);
+        assertTrue(bubble.xSpeed < 40);
+        bubble.xSpeed = 20;
+        bubble.update(0.1f);
+        verify(bubble).getOutOfGame();
+    }
+
+    @Test
+    public void testUpdateLifespan() {
+        bubble.update(0.1f);
+        assertFalse(bubble.remove);
+        bubble.update(BubbleObject.BUBBLE_LIFESPAN);
+        assertTrue(bubble.remove);
+    }
+
+
+
+
+}
