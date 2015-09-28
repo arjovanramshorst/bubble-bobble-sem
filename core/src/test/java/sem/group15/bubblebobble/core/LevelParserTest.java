563ae68de13d7182b29b7446c9a147584daa3cdd
Daan Vermunt <daanvermunt@gmail.com>
2015-09-28 23:26:34 +0200

    fixed build failure

--- core/src/test/java/sem/group15/bubblebobble/core/LevelParserTest.java
+++ core/src/test/java/sem/group15/bubblebobble/core/LevelParserTest.java
@@ -1,20 +1,13 @@
 package sem.group15.bubblebobble.core;
 
 import com.badlogic.gdx.files.FileHandle;
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 import static org.mockito.Mockito.*;
-import org.mockito.Mockito;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Scanner;
 
 import static org.junit.Assert.assertTrue;
 
