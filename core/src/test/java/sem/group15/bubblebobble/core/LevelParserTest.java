720673a81716d07c8955d9762034223e0a036710
Daan Vermunt <daanvermunt@gmail.com>
2015-09-28 23:14:46 +0200

    added many tests and got coverage up a bit

---
+++ core/src/test/java/sem/group15/bubblebobble/core/LevelParserTest.java
@@ -0,0 +1,7 @@
+package sem.group15.bubblebobble.core;
+
+/**
+ * Created by Marketing Lorre on 28-9-2015.
+ */
+public class LevelParserTest {
+}
---
+++ core/src/test/java/sem/group15/bubblebobble/core/objects/FloorObjectTest.java
@@ -0,0 +1,7 @@
+package sem.group15.bubblebobble.core.objects;
+
+/**
+ * Created by Marketing Lorre on 28-9-2015.
+ */
+public class FloorObjectTest {
+}
