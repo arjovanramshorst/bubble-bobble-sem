df968f80f476b886baefb6327024cb155557f901
Arjo <arjovanramshorst@gmail.com>
2015-10-08 16:28:53 +0200

    Initial factories for EnemyObject <organisation

---
+++ core/src/main/java/sem/group15/bubblebobble/core/factories/EnemyFactory.java
@@ -0,0 +1,11 @@
+package sem.group15.bubblebobble.core.factories;
+
+import sem.group15.bubblebobble.core.objects.EnemyObject;
+import sem.group15.bubblebobble.core.objects.GameObject;
+
+/**
+ * Created by arjo on 8-10-15.
+ */
+public interface EnemyFactory {
+    public EnemyObject createObject();
+}
---
+++ core/src/main/java/sem/group15/bubblebobble/core/factories/SimpleEnemyFactory.java
@@ -0,0 +1,14 @@
+package sem.group15.bubblebobble.core.factories;
+
+import sem.group15.bubblebobble.core.objects.EnemyObject;
+
+/**
+ * Created by arjo on 8-10-15.
+ */
+public class SimpleEnemyFactory implements EnemyFactory {
+
+    @Override
+    public EnemyObject createObject() {
+        return null;
+    }
+}
