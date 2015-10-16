a48d7da395c4407f29e078a42a01d89cf95920dd
wouterz <Zirkzee@hotmail.com>
2015-10-16 22:56:57 +0200

    some tests for gameController, was hard to test

--- core/src/main/java/sem/group15/bubblebobble/core/GameController.java
+++ core/src/main/java/sem/group15/bubblebobble/core/GameController.java
@@ -11,8 +11,15 @@ import java.io.IOException;
  * Created by arjo on 7-9-15.
  */
 public class GameController {
+
     private Player player;
+
     private Level currentLevel;
+
+    public void setLevelRenderer(LevelRenderer levelRenderer) {
+        this.levelRenderer = levelRenderer;
+    }
+
     private LevelRenderer levelRenderer;
     private int currentLevelNumber;
     private static final int MAX_LEVEL = 3;
@@ -41,7 +48,7 @@ public class GameController {
     /**
      * Resets the GameController so a new game can be started.
      */
-    private void resetController() {
+    protected void resetController() {
         player = new Player(PLAYER_XY_SPAWN, PLAYER_XY_SPAWN);
         state = GameState.PLAY;
         currentLevelNumber = 1;
@@ -50,6 +57,7 @@ public class GameController {
     /**
      * Main game loop. Calls the appropriate method to handle the current game
      * state.
+     *
      * @param elapsed time elapsed since last frame.
      */
     public final void run(final float elapsed) {
@@ -72,7 +80,7 @@ public class GameController {
     /**
      * Is called when the gamestate is new.
      */
-    private void handleStateNew() {
+    protected void handleStateNew() {
         levelRenderer.renderNew();
         if (checkForStartKey()) {
             state = GameState.PLAY;
@@ -82,6 +90,7 @@ public class GameController {
 
     /**
      * Handles the game when it is in the 'play' state.
+     *
      * @param elapsed elapsed time since last frame.
      */
     private void handleStatePlay(float elapsed) {
@@ -102,7 +111,7 @@ public class GameController {
     /**
      * Is called when the gamestate is paused.
      */
-    private void handleStatePause() {
+    protected void handleStatePause() {
         levelRenderer.renderPause();
         if (checkForStartKey()) {
             state = GameState.PLAY;
@@ -112,7 +121,7 @@ public class GameController {
     /**
      * Is called when the gamestate is Lost.
      */
-    private void handleStateLost() {
+    protected void handleStateLost() {
         levelRenderer.renderLost(currentLevelNumber);
         if (checkForStartKey()) {
             resetController();
@@ -122,14 +131,16 @@ public class GameController {
 
     /**
      * Returns true if the start key is pressed (enter for now).
+     *
      * @return true if enter is pressed.
      */
-    private boolean checkForStartKey() {
+    protected boolean checkForStartKey() {
         return Gdx.input.isKeyPressed(Input.Keys.ENTER);
     }
 
     /**
      * Returns true if the pause key is pressed (escape for now).
+     *
      * @return true if escape is pressed.
      */
     private boolean checkForPauseKey() {
@@ -138,6 +149,7 @@ public class GameController {
 
     /**
      * Initiates all objects the game needs.
+     *
      * @param levelNumber level number.
      */
     public final void startLevel(final int levelNumber) {
@@ -153,9 +165,65 @@ public class GameController {
 
     /**
      * Checks if the game is lost.
+     *
      * @return true if the player lost the game.
      */
-    private boolean checkForLose() {
+    protected boolean checkForLose() {
         return !player.isAlive();
+    }
+
+    /**
+     * Get player.
+     *
+     * @return player
+     */
+    public Player getPlayer() {
+        return player;
+    }
+
+    /**
+     * Set Player.
+     *
+     * @param player player to be set as player
+     */
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    /**
+     * Get level number.
+     *
+     * @return currentLevelNumeber
+     */
+    public int getCurrentLevelNumber() {
+        return currentLevelNumber;
+    }
+
+    /**
+     * Set currentLevelNumber.
+     *
+     * @param currentLevelNumber number to set currentLevelNumber to
+     */
+    public void setCurrentLevelNumber(int currentLevelNumber) {
+        this.currentLevelNumber = currentLevelNumber;
+    }
+
+    /**
+     * Get state of the controller.
+     *
+     * @return state
+     */
+    public GameState getState() {
+        return state;
+    }
+
+    /**
+     * Set the state of the controller.
+     *
+     * @param state new state
+     */
+    public void setState(GameState state) {
+        this.state = state;
+
     }
 }
