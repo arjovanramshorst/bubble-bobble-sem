6d9c3f0dc4db834325f3e7fb650d32895b63dd2c
Eric Dammeijer <ericdammeyer@gmail.com>
2015-10-23 19:24:17 +0200

    fix bug and repaired test <bug, organisation, testing

--- core/src/main/java/sem/group15/bubblebobble/core/Assets.java
+++ core/src/main/java/sem/group15/bubblebobble/core/Assets.java
@@ -164,6 +164,11 @@ public final class Assets {
      * Getter for highScore
      */
     public static int getHighScore() {
-        return prefs.getInteger("highScore");
+        try {
+            return prefs.getInteger("highScore");
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return 0;
     }
 }
